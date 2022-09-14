afcd93425defd49cc96f82e966d9171e97347c03
tawnchan <tawnchan@gmail.com>
2022-09-14 13:52:56 -0700

    completed second challenge

--- jest-david-tawn/jest.test.js
+++ jest-david-tawn/jest.test.js
@@ -29,9 +29,43 @@ const areYouTired = (string) => {
 }
 
 // Write the test for a function that returns "relax" if you are stressed and "keep going" if you are not stressed.
+// 1. describe "areYouStressed" and map function structure.
+// 2. it returns "'relax' if you are stressed, 'keep going' if you are not stressed"
+// 3. write out expect outcomes
+// 4. run in terminal, $ yarn jest
+
+describe("areYouStressed", () => {
+  it("returns 'relax' if you are stressed, 'keep going' if you are not stressed", () => {
+    expect(areYouStressed("yes")).toEqual("relax")
+    expect(areYouStressed("no")).toEqual("keep going")
+  })
+})
+// ReferenceError: areYouStressed is not defined
+
 // Create the function that will make the test pass.
+  // 1. Declare a function areYouStressed a string and structure function.
+  // 2. use if/else if conditional statements to determine if string is strictly yes or no and return a cooresponding outcome.
+  // 3. retes, $ yarn jest
+
+const areYouStressed = (string) => {
+  if(string === "yes") {
+    return "relax"
+  } else if(string === "no") {
+    return "keep going"
+  } else {
+    return "you're brokeneth"
+  }
+}
+//  PASS  ./jest.test.js
+
+
 // Write the test for a function that returns "in budget" if a price is lower than $300.
+
+
+
 // Create the function that will make the test pass.
+
+
 // Write the test for a function that takes in two numbers and returns the smaller number.
 // Create the function that will make the test pass.
 // Write the test for a function that takes in one numbers and returns whether the number is odd.
