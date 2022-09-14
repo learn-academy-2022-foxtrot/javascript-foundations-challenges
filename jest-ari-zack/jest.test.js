0aef5faba6c13c6d695b929c5867308e541b2467
ZackRohde <zdrohde@outlook.com>
2022-09-14 13:47:25 -0700

    1-3 Challenges Completed

--- jest-ari-zack/jest.test.js
+++ jest-ari-zack/jest.test.js
@@ -40,14 +40,81 @@ Steps:
 // 1. Write the test for a function that returns "drink coffee" if you are tired and "keep working" if you are not tired.
 // Create the function that will make the test pass.
 
+describe("areYouTired", () => {
+  it("returns drink coffee if you are tired and keep working if you are not tired.", () => {
+    let isTired = "yes"
+    let notTired = "no"
+    let result1 = "drink coffee"
+    let result2 = "keep working"
+    expect(areYouTired(isTired)).toEqual(result1)
+    expect(areYouTired(notTired)).toEqual(result2)
+  })
+})
+
+const areYouTired = (string) => {
+  if(string === "yes"){
+  return "drink coffee" 
+} else if (string === "no") {
+  return "keep working" 
+} else {
+  return "Error"
+}
+}
+
 // 2. Write the test for a function that returns "relax" if you are stressed and "keep going" if you are not stressed.
 // Create the function that will make the test pass.
+
+describe("areYouStressed", () => {
+  it(`returns "relax" if you are stressed and "keep going" if you are not stressed.`, () => {
+    let notStressed = "no"
+    let isStressed = "yes"
+    let result1 = "keep going"
+    let result2 = "relax"
+    expect(areYouStressed(notStressed)).toEqual(result1)
+    expect(areYouStressed(isStressed)).toEqual(result2)
+  })
+})
+
+const areYouStressed = (string) => {
+  if(string === "yes") {
+    return "relax"
+  } else if (string === "no") {
+    return "keep going"
+  } else {
+    return "Error"
+  }
+}
 
 // 3. Write the test for a function that returns "in budget" if a price is lower than $300.
 // Create the function that will make the test pass.
 
+describe("budgetCheck", () => {
+  it(`returns "in budget" if a price is lower than $300.`, () => {
+    let price1 = 200
+    let price2 = 300
+    let price3 = 400
+    result1 = "in budget"
+    result2 = "out of budget"
+    expect(budgetCheck(price1)).toEqual(result1)
+    expect(budgetCheck(price2)).toEqual(result1)
+    expect(budgetCheck(price3)).toEqual(result2)
+  })
+})
+
+const budgetCheck = (number) => {
+  if(number <= 300) {
+    return "in budget"
+  } else if (number > 300) {
+    return "out of budget" 
+  } else {
+    return "Error"
+  }
+}
+
 // 4. Write the test for a function that takes in two numbers and returns the smaller number.
 // Create the function that will make the test pass.
+
+
 
 // 5. Write the test for a function that takes in one numbers and returns whether the number is odd.
 // Create the function that will make the test pass.
