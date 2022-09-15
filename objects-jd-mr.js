cad267745930bc5cff980914042900ca6a3cf34b
rhodesmt <mrhodes415@gmail.com>
2022-09-15 12:03:10 -0700

    1-6 Updated

--- objects-jd-mr.js
+++ objects-jd-mr.js
@@ -9,18 +9,18 @@ const person = {
 }
 
 // Write the code that accesses the first name of the person object.
-// console.log(person.firstName)
+console.log(person.firstName)
 
 // let key = "firstName"
 // // console.log(person[key])
 
 // // Write the code that accesses the last name of the person object.
-// console.log(person.lastName)
+console.log(person.lastName)
 
 // // Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
 person.homePlanet = "Earth"
-// console.log(person.homePlanet)
-// console.log(person)
+console.log(person.homePlanet)
+console.log(person)
 
 // Update the person object with a method that logs "Arthur Dent is from planet Earth".
 person.displayDetails = function () {
