2bf9cc5513aca3631912eb3922a9ec7f42292b87
Goran SD <g.cvetic23@gmail.com>
2022-09-15 12:01:30 -0700

    first half

--- js-objects-gc-kl-ac.js
+++ js-objects-gc-kl-ac.js
@@ -1,32 +1,65 @@
 // Consider this variable:
 
-const person = {
-  firstName: "Arthur",
-  lastName: "Dent",
-};
+// const person = {
+//   firstName: "Arthur",
+//   lastName: "Dent",
+//   homePlanet: "Earth",
+//   getData: function () {
+//     return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`;
+//   },
+// };
+
 // Write the code that accesses the first name of the person object.
+// console.log(person.firstName);
 
 // Write the code that accesses the last name of the person object.
+// console.log(person.lastName);
+
 // Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+// console.log(person.homePlanet);
+
 // Update the person object with a method that logs "Arthur Dent is from planet Earth".
+// console.log(person.getData());
+
 // Consider this variable:
 
 // const product = {
 //   name: "chair",
-//   price: 24.99
-// }
+//   price: 24.99,
+// };
+
 // Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
+// const describeProduct = (product) => {
+//   return `The product is the ${product.name}. It costs $${product.price}`;
+// };
+
+// console.log(describeProduct(product));
+
 // Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
+// const totalWithTax = (product) => {
+//   return product.price + product.price * 0.07;
+// };
+
+// console.log(totalWithTax(product));
+
 // Consider this variable:
 
-// const lunch = {
-//   name: "PB and Banana",
-//   type: "sandwich",
-//   ingredients: ["bread", "peanut butter", "banana"]
-// }
+const lunch = {
+  name: "PB and Banana",
+  type: "sandwich",
+  ingredients: ["bread", "peanut butter", "banana"],
+};
 // Write the code that accesses the ingredients property.
+// console.log(lunch.ingredients);
+
 // Write the code that access the third ingredient of the lunch object.
+// console.log(lunch.ingredients[2]);
+
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+// const pbRecipe = (lunch) => {
+//   return `The ingredients for PB and Banana sandwich are ${lunch.ingredients[0]}, ${lunch.ingredients[1]}, and ${lunch.ingredients[2]}.`;
+// };
+// console.log(pbRecipe(lunch));
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
 // Consider this variable:
 
