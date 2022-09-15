79aed88794e856fa450928b2d49171c7ac4c1c36
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-15 13:01:23 -0700

    not done

---
+++ objects-chris-heath.js
@@ -0,0 +1,145 @@
+/*
+Create a function called ___ that will ___
+input: keys in the object
+output: Arthur
+
+Step 1: identify the keys
+Step 2: console.log the firstName and lastName
+Step 3: 
+Step 4:
+*/
+
+const person = {
+    firstName: "Arthur",
+    lastName: "Dent",
+    homePlanet: "Earth",
+    addUp: function () {
+        return '${this.firstName, this.lastName} is from ${this.homePlanet}' 
+    }
+    //let homePlanet = { "earth"}
+    //--> "Arthur Dent is from planet Earth"
+  }
+
+  console.log(person.firstName)
+  console.log(person.lastName)
+  console.log(person.addUp())
+  //console.log()
+//   Write the code that accesses the first name of the person object.
+//   Write the code that accesses the last name of the person object.
+//   Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+//   Update the person object with a method that logs "Arthur Dent is from planet Earth".
+  
+/*
+Create a function called ___ that will ___
+input: 
+output: 
+
+Step 1: 
+Step 2:
+Step 3: 
+Step 4:
+*/
+
+// const product = {
+//     name: "chair",
+//     price: 24.99
+//   }
+//   Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
+//   Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
+
+/*
+Create a function called ___ that will ___
+input: 
+output: 
+
+Step 1: 
+Step 2:
+Step 3: 
+Step 4:
+*/
+
+// const animals = [
+//     { name: "Waffles", type: "dog", age: 7 },
+//     { name: "Fluffy", type: "cat", age: 14 },
+//     { name: "Spelunky", type: "dog", age: 4 },
+//     { name: "Hank", type: "cat", age: 11 }
+//   ]
+//   Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+//   Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+//   Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+//   Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+
+/*
+Create a function called ___ that will ___
+input: 
+output: 
+
+Step 1: 
+Step 2:
+Step 3: 
+Step 4:
+*/
+
+// const author = {
+//     name: "H. G. Wells",
+//     genre: "science fiction"
+//   }
+//   Write the code that destructures the author object so that the following code snippet will run successfully:
+//   console.log(`${name} is a ${genre} author`)
+  // output: "H. G. Wells is a science fiction author"
+
+/*
+Create a function called ___ that will ___
+input: 
+output: 
+
+Step 1: 
+Step 2:
+Step 3: 
+Step 4:
+*/
+
+//   const pokeOne = {
+//     species: "Charmandar",
+//     pokemon_type: "Fire"
+//   }
+
+/*
+Create a function called ___ that will ___
+input: 
+output: 
+
+Step 1: 
+Step 2:
+Step 3: 
+Step 4:
+*/
+
+//   const pokeTwo = {
+//     species: "Magikarp",
+//     pokemon_type: "Water"
+//   }
+//   Create a function called describePokemon that take an object like the ones above and uses destructuring to return a description of the Pokemon so that the following code snippet will run successfully:
+//   console.log(describePokemon(pokeOne))
+//   // output: "Charmandar is a Fire pokemon"
+//   console.log(describePokemon(pokeTwo))
+//   // output: "Magikarp is a Water pokemon"
+
+/*
+Create a function called ___ that will ___
+input: 
+output: 
+
+Step 1: 
+Step 2:
+Step 3: 
+Step 4:
+*/
+
+// const triangleDimensions = {
+//     base: 2,
+//     height: 5
+//   }
+//   Modify the triangleDimensions object to have a method that returns the area of the triangle.
+//   Write the code that will update the base to be the value of 6.
+  
