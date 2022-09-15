0c0ec6a5b86439c26505e21f84ce4641b7033a00
ZackRohde <zdrohde@outlook.com>
2022-09-15 11:30:56 -0700

    Initial Commit, First Challenge Done

---
+++ objects-zack-michy.js
@@ -0,0 +1,22 @@
+const person = {
+  firstName: "Arthur",
+  lastName: "Dent",
+  homePlanet: "Earth",
+  hesFrom: "Arthur Dent is from planet Earth"
+}
+// Write the code that accesses the first name of the person object.
+
+// console.log(person.firstName)
+
+// Write the code that accesses the last name of the person object.
+
+// console.log(person.lastName)
+
+// Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+
+// console.log(person.homePlanet)
+
+// Update the person object with a method that logs "Arthur Dent is from planet Earth".
+
+// console.log(person.hesFrom)
+
