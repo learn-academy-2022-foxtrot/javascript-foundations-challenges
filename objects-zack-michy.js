e803d407dfd4d3c2db8123a1b35a1b76d023155c
ZackRohde <zdrohde@outlook.com>
2022-09-15 11:58:32 -0700

    Challenge 1.5 Finished

--- objects-zack-michy.js
+++ objects-zack-michy.js
@@ -1,9 +1,9 @@
-const person = {
-  firstName: "Arthur",
-  lastName: "Dent",
-  homePlanet: "Earth",
-  hesFrom: "Arthur Dent is from planet Earth"
-}
+// const person = {
+//   firstName: "Arthur",
+//   lastName: "Dent",
+//   homePlanet: "Earth",
+//   hesFrom: "Arthur Dent is from planet Earth"
+// }
 // Write the code that accesses the first name of the person object.
 
 // console.log(person.firstName)
@@ -20,3 +20,27 @@ const person = {
 
 // console.log(person.hesFrom)
 
+// const product = {
+//   name: "chair",
+//   price: 24.99,
+//   description: "The product is a chair. It costs $24.99"
+// }
+// Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
+
+// const describeProduct = (productArray) => {
+//   return productArray.map(element => {
+//     return element.description
+//   })
+// }
+// console.log(product.description)
+
+// Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7 % sales tax rounded to two decimals.
+const product = {
+  name: "chair",
+  price: 24.99,
+  description: "The product is a chair. It costs $24.99"
+}
+const totalWithTax = (productArgument) => {
+  return productArgument.price * 7 % 2
+}
+console.log(totalWithTax(product))
